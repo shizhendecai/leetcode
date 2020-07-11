5c69d7e4baff41e5f6af58cedf1ba83073243f9c
shizhendecai <yishujl@outlook.com>
2020-07-11 12:35:35 +0800

    2020/7/11

--- 14. 最长公共前缀/index.js
+++ 14. 最长公共前缀/index.js
@@ -1,7 +1,30 @@
+/**编写一个函数来查找字符串数组中的最长公共前缀。
+
+如果不存在公共前缀，返回空字符串 ""。 */
+
 /**
  * @param {string[]} strs
  * @return {string}
  */
 var longestCommonPrefix = function(strs) {
-
+    let res= strs[0]
+    for (let i = 0; i < strs.length; i++) {
+        let str = strs[i];
+        let len = res.length<=str.length?str.length:res.length
+        let j =0;
+        let temp = ""
+        while(j<len){
+            if(res[j] == str[j]){
+                temp += str[j]
+                j++
+            }else{
+                res = temp
+                break
+            }
+        }
+    }
+    return res==undefined?"":res
 };
+
+console.log(longestCommonPrefix(["flower","flow","flight"]))
+// console.log(longestCommonPrefix(["aa","a"]))
